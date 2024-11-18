cfd0dd3a279f9a5d9d18158cd9d6858b0b386176
Giannisell <76254256+Giannisell@users.noreply.github.com>
2024-11-17 23:18:19 -0300

    Delete HistorialMedico directory

--- HistorialMedico/src/main/java/com/gestion/medica/HistorialMedico/PacienteNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.gestion.medica.HistorialMedico;
-
-public class PacienteNoEncontradoException extends RuntimeException {
-    public PacienteNoEncontradoException(String message) {
-        super(message);
-    }
-}
--- HistorialMedico/src/main/java/com/gestion/medica/HistorialMedico/PacienteYaExisteException.java
+++
@@ -1,7 +0,0 @@
-package com.gestion.medica.HistorialMedico;
-
-public class PacienteYaExisteException extends RuntimeException {
-    public PacienteYaExisteException(String message) {
-        super(message);
-    }
-}
